00f6c5b5686dcc9ec8ef8103d09b75e55c0e2bd3
Deyvison Nogueira <93131818+deyvisonogueira@users.noreply.github.com>
2022-08-17 20:04:41 -0300

    Delete Tech Web directory

--- Tech Web/aula1.js
+++
@@ -1,7 +0,0 @@
-function mudaCor() {
-    title.style.color = 'red';
-    paragraph.style.color = 'red';     
-}
-
-title.addEventListener('click', mudaCor(title));
-paragraph.addEventListened('click', mudaCor(paragraph));
